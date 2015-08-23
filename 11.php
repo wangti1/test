49136a1ac7a603d861efa388a432703819717ed9
wangti1 <517484954@qq.com>
2015-08-23 10:30:20 +0800

    add文件

---
+++ 11.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wd
+ * Date: 2015/8/11
+ * Time: 20:42
+ */
+header("content-type:text/html;charset=utf8");
+echo $_GET["user"];
+echo $_GET["password"];
+
+?>
